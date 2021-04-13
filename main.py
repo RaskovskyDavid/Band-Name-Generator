558219ff68d8acefb6a74e1f94eebc29b16af849
RaskovskyDavid <>
2021-04-13 11:33:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Write your code below this line 👇
+print("Welcome to Band Name Generator")
+prstreet = input("what is the name of your city \n")
+petname = input("what is the name of your pet \n")
+print("Your band name is " + prstreet +" " + petname)
+
+
+
+
+
+
+
+
+
